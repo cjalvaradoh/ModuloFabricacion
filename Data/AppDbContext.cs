35c706714ab8bfd7ca593bcd8497d97e78e531f2
Ces <ces.alvahernandez@gmail.com>
2025-03-27 17:01:38 -0600

    Configuraciones Generales

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using caobaModeloFabricacion.Models;
+namespace caobaModeloFabricacion.Data
+{
+    public class AppDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+           : base(options)
+        {
+        }
+       // public DbSet<>  { get; set; }
+        //public DbSet<>  { get; set; }
+        //public DbSet<>  { get; set; }
+        //public DbSet<>  { get; set; }
+        //public DbSet<>  { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+        }
+
+    }
+}
